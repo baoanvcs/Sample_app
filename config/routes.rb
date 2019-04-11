4610d06c747140ef69161427c80ca5acabdae1ff
baoanvcs@gmail.com <YOUR@EMAIL.com>
2019-04-11 16:45:52 +0700

    chapter_4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
-MyApp::Application.routes.draw do
-root :to => 'static_pages#home'
-
-match '/help',    :to => 'static_pages#help'
-match '/about',   :to => 'static_pages#about'
-match '/contact', :to =>'static_pages#contact'
-
-
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "static_pages/home"
+    get "static_pages/help"
+    get "static_pages/about"
+  end
 end
