b4e0d1f89a92b8d79087227cd7de4ed38cd5af6f
baoanvcs@gmail.com <YOUR@EMAIL.com>
2019-04-11 15:28:07 +0700

    chapter_13

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      remember_password user
+      redirect_to user # user_url(user)
+    else
+      flash[:danger] = t"static_pages.login.error_message"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  def remember_password user
+    checked = params[:session][:remember_me]
+    checked == Settings.user.remember_user ? remember(user) : forget(user)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,52 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :find_param, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.order(:name).page params[:page]
   end
 
-  def show
-    @user = User.find params[:id]
-    rescue ActiveRecord::RecordNotFound
-    render "errors/error_404"
+  def new
+    @user = User.new
   end
 
   def create
     @user = User.new user_params
 
     if @user.save
-      flash[:success] = t("Welcome_to_the_Sample_App!")
+      log_in @user
+      flash[:success] = t("static_pages.home.welcome")
       redirect_to @user
     else
-      flash[:success] = t("Create product Fail! >.<")
       render :new
     end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    @microposts = @user.microposts.order("created_at DESC").page params[:page]
+  end
+
+  def edit
+    @user = find_param
+  end
+
+  def update
+    @user = find_param
+    if @user.update(user_params)
+      flash[:success] = t("static_pages.users.new.profile_updated")
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find_param.destroy
+    flash[:success] = t("static_pages.users.delete.deleted")
+    redirect_to users_url
   end
 
   private
@@ -26,5 +54,21 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t("static_pages.users.new.login_require")
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
